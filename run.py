50e36e1a1d045b682762dc90c22a92bfd988d217
Gennadiy Tarada <gennadiy@tarada.xyz>
2021-08-15 16:59:32 +0500

    🎈collect data with dataclasses

--- run.py
+++ run.py
@@ -1,22 +1,83 @@
 # -*- coding: utf-8 -*-
 
-from nornir import InitNornir
-from nornir_utils.plugins.functions import print_result
-from nornir_scrapli.tasks import send_commands
-from nornir.core.filter import F
+from dataclasses import dataclass, field
+
 import click
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir.core.task import Task
+from nornir_scrapli.tasks import send_command
+from nornir_scrapli.functions import print_structured_result
+from nornir_utils.plugins.functions import print_result
+from typing import List
+from rich import print
 
 
-def get_report(task):
+# @dataclass
+# class DHCPSnooping:
+
+# @dataclass
+# class PortSecurity:
+
+
+@dataclass
+class Interface:
+    name: str
+    duplex: str
+    speed: str
+    type: str
+    vlan: str
+    status: str
+    description: str
+    input_errors: int
+
+
+#     dhcp_snooping: DHCPSnooping
+#     port_security: PortSecurity
+
+
+@dataclass
+class Switch:
+    hostname: str
+    ip_address: str
+    interfaces: List[Interface]
+
+
+# TODO Move to plugins
+def get_status_data(task: Task) -> Switch:
     if task.host.data["vendor"] == "cisco" and task.host.data["driver"] == "cli-ssh":
         task.host.platform = "cisco_iosxe"
-        return task.run(
-            send_commands,
-            commands=[
-                "show interfaces status | exclude routed|trunk",
-                "show mac address-table secure",
-            ],
-        )
+        # Get interfaces status data
+        # TODO Move to another function
+        command_string = "show interfaces status"
+        nr_task_result = task.run(send_command, command=command_string)
+        struct_output = nr_task_result[0].scrapli_response.genie_parse_output()
+        interfaces = []
+        for interface_name in struct_output["interfaces"].keys():
+            # Check for blank description
+            if "name" not in struct_output["interfaces"][interface_name]:
+                struct_output["interfaces"][interface_name]["name"] = ""
+            # Get interface detail data
+            # TODO Move to another function
+            int_command_string = f"show interface {interface_name}"
+            int_nr_task_result = task.run(send_command, command=int_command_string)
+            int_struct_output = int_nr_task_result[
+                0
+            ].scrapli_response.genie_parse_output()
+            # Fill interfaces list
+            interfaces.append(
+                Interface(
+                    interface_name,
+                    struct_output["interfaces"][interface_name]["duplex_code"],
+                    struct_output["interfaces"][interface_name]["port_speed"],
+                    struct_output["interfaces"][interface_name]["type"],
+                    struct_output["interfaces"][interface_name]["vlan"],
+                    struct_output["interfaces"][interface_name]["status"],
+                    struct_output["interfaces"][interface_name]["name"],
+                    int_struct_output[interface_name]["counters"]["in_errors"],
+                )
+            )
+    return Switch(task.host.name, task.host.hostname, interfaces)
 
 
 @click.command()
@@ -24,8 +85,11 @@ def get_report(task):
 def main(filter: str):
     nr = InitNornir(config_file="nornir_config.yaml")
     hosts = nr.filter(F(name__contains=filter))
-    output = hosts.run(get_report)
-    print_result(output)
+    output = hosts.run(get_status_data)
+    switches = []
+    for switch in output.keys():
+        switches.append(output[switch].result)
+    print(switches)
 
 
 if __name__ == "__main__":
