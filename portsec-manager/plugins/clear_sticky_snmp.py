f819bee18f690ce20f1e133a9d63886342ac88fe
Tarada Gennadiy <gtarada@vostok-electra.ru>
2021-09-17 16:54:38 +0500

    some renames

---
+++ portsec-manager/plugins/clear_sticky_snmp.py
@@ -0,0 +1,13 @@
+"""
+
+cpsIfClearSecureMacAddresses  OBJECT-TYPE
+        SYNTAX        ClearSecureMacAddrType
+        MAX-ACCESS    read-write
+        STATUS        current
+        DESCRIPTION   "This objects allows the user to delete
+                      secure MAC addresses based on the type specified."
+
+        ::= { cpsIfConfigEntry 14 }
+
+
+"""
