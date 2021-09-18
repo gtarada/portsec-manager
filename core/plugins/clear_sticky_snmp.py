c4a04763d27494f1af08f84a95d149d157c8915b
Tarada Gennadiy <gtarada@vostok-electra.ru>
2021-09-18 22:18:55 +0500

    clear snmpset request

--- core/plugins/clear_sticky_snmp.py
+++ core/plugins/clear_sticky_snmp.py
@@ -1,13 +1,134 @@
-"""
+# -*- coding: utf-8 -*-
 
-cpsIfClearSecureMacAddresses  OBJECT-TYPE
-        SYNTAX        ClearSecureMacAddrType
-        MAX-ACCESS    read-write
-        STATUS        current
-        DESCRIPTION   "This objects allows the user to delete
-                      secure MAC addresses based on the type specified."
+from nornir.core.task import Task
+from netaddr import EUI, mac_unix
+from pysnmp.hlapi import (
+    setCmd,
+    SnmpEngine,
+    CommunityData,
+    UdpTransportTarget,
+    ContextData,
+    ObjectType,
+    ObjectIdentity,
+    getCmd,
+    nextCmd,
+)
+from pysnmp.smi import builder
+import os
 
-        ::= { cpsIfConfigEntry 14 }
+
+def snmpset_clear_iface(hostname: str, community: str, ifindex: int) -> None:
+    snmpEngine = SnmpEngine()
+    snmpBuilder = snmpEngine.getMibBuilder()
+    snmpBuilder.addMibSources(builder.DirMibSource(os.path.join("mibs")))
+    g = setCmd(
+        snmpEngine,
+        CommunityData(community),
+        UdpTransportTarget((hostname, 161)),
+        ContextData(),
+        ObjectType(
+            ObjectIdentity(
+                # "CISCO-PORT-SECURITY-MIB", "cpsIfClearSecureMacAddresses", ifindex
+                "CISCO-PORT-SECURITY-MIB",
+                "cpsIfClearSecureAddresses",
+                ifindex,
+            ),
+            1,
+        ),
+    )
+    errorIndication, errorStatus, errorIndex, varBinds = next(g)
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print(
+            "%s at %s"
+            % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
+            )
+        )
+    else:
+        for varBind in varBinds:
+            print(" = ".join([x.prettyPrint() for x in varBind]))
 
 
-"""
+def snmpget_last_mac_address(hostname: str, community: str, ifindex: int) -> EUI:
+    snmpEngine = SnmpEngine()
+    snmpBuilder = snmpEngine.getMibBuilder()
+    snmpBuilder.addMibSources(builder.DirMibSource(os.path.join("mibs")))
+    g = getCmd(
+        snmpEngine,
+        CommunityData(community),
+        UdpTransportTarget((hostname, 161)),
+        ContextData(),
+        ObjectType(
+            ObjectIdentity(
+                "CISCO-PORT-SECURITY-MIB", "cpsIfSecureLastMacAddress", ifindex
+            )
+        ),
+    )
+
+    errorIndication, errorStatus, errorIndex, varBinds = next(g)
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print(
+            "%s at %s"
+            % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
+            )
+        )
+    else:
+        for varBind in varBinds:
+            output = [x.prettyPrint() for x in varBind][1]
+    return EUI(output, version=48, dialect=mac_unix)
+
+
+def snmpget_secure_mac_address(hostname: str, community: str, ifindex: int) -> EUI:
+    snmpEngine = SnmpEngine()
+    snmpBuilder = snmpEngine.getMibBuilder()
+    snmpBuilder.addMibSources(builder.DirMibSource(os.path.join("mibs")))
+    g = nextCmd(
+        snmpEngine,
+        CommunityData(community),
+        UdpTransportTarget((hostname, 161)),
+        ContextData(),
+        ObjectType(
+            ObjectIdentity("CISCO-PORT-SECURITY-MIB", "cpsSecureMacAddrType", ifindex)
+        ),
+    )
+    output = []
+    errorIndication, errorStatus, errorIndex, varBinds = next(g)
+    if errorIndication:
+        print(errorIndication)
+    elif errorStatus:
+        print(
+            "%s at %s"
+            % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
+            )
+        )
+    else:
+        for varBind in varBinds:
+            right_value = [x.prettyPrint() for x in varBind][0]
+            address = right_value[right_value.find('"') + 1 : len(right_value) - 1]
+            output.append(EUI(address, version=48, dialect=mac_unix))
+    return output
+
+
+def clear_sticky_snmp(task: Task, iface: str) -> None:
+    ifindex = 30
+    last_mac = snmpget_last_mac_address(
+        task.host.hostname, task.host.data["community"], ifindex
+    )
+    secure_macs = snmpget_secure_mac_address(
+        task.host.hostname, task.host.data["community"], ifindex
+    )
+    found = False
+    for secure_mac in secure_macs:
+        if last_mac == secure_mac:
+            found = True
+    if not found:
+        snmpset_clear_iface(task.host.hostname, task.host.data["community_rw"], ifindex)
--- portsec-manager.py
+++ portsec-manager.py
@@ -1,36 +1,56 @@
 # -*- coding: utf-8 -*-
 
+from typing import Dict
+
 import click
 from nornir import InitNornir
+from nornir.core import Nornir
 from nornir.core.filter import F
+from nornir.core.inventory import Hosts
 
-from core.plugins.get_status_data import get_status_data
 from core.plugins.console_print_switch import console_print_switch
+from core.plugins.get_status_data import get_status_data
+
+from core.plugins.clear_sticky_snmp import clear_sticky_snmp
 
 
-def get_status(filter: str) -> None:
-    nr = InitNornir(config_file="nornir_config.yaml")
-    hosts = nr.filter(F(name__contains=filter))
+def get_status(nr: Nornir) -> None:
     # TODO add progressbar
-    output = hosts.run(get_status_data)
+    output = nr.run(get_status_data)
     switches = {}
     for switch in output.keys():
         switches[switch] = output[switch].result
         console_print_switch(switches[switch])
 
 
-def clear_sticky(clear: str) -> None:
-    pass
+def clear_sticky(clear: str, nr: Nornir) -> None:
+    # TODO Interface or MAC address check
+    iface = clear
+    # TODO Check device properties and model
+    output = nr.run(clear_sticky_snmp, iface=clear)
+
+
+def inventory_single_host(nr: Nornir, filter: str) -> Hosts:
+    nr_filter_hosts = nr.filter(
+        F(
+            name__contains=filter,
+        )
+    )
+    nr_hosts_list = [x for x in nr_filter_hosts.inventory.hosts.keys()]
+    nr_host = nr_filter_hosts.inventory.hosts[nr_hosts_list[0]]
+    return Hosts({str(nr_host): nr_host})
 
 
 @click.command()
 @click.option("--filter", required=True, prompt=True)
 @click.option("--clear", type=str)
 def main(filter: str, clear: str) -> None:
+    nr = InitNornir(config_file="nornir_config.yaml")
+    nr.inventory.hosts = inventory_single_host(nr, filter)
+
     if clear:
-        clear_sticky(clear)
-    else:
-        get_status(filter)
+        clear_sticky(clear, nr)
+    get_status(nr)
 
 
 if __name__ == "__main__":
